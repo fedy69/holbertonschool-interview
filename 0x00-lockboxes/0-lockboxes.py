9a2f5ecbae869b87e27faa1af7186fb699197daf
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-08-05 04:04:20 +0100

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,15 +3,30 @@
 
 
 def canUnlockAll(boxes):
-    u = [0]
-    if len(boxes) == 0:
-        return True
-    for box in u:
-        for key in boxes[box]:
-            if key not in u:
-                if key < len(boxes):
-                    u.append(key)
+    """_summary_
+    Args:
+        boxes (_type_): list of lists to check
+    Returns:
+        _type_: _description_
+    """
+    canUnlockAll = False
+    keys = {0: True}
+    n_boxes = len(boxes)
+    while (True):
 
-    if len(u) == len(boxes):
-        return True
-    return False
+        n_keys = len(keys)
+
+        for i in range(len(boxes)):
+            if boxes[i] and keys.get(i, False):
+                for j in boxes[i]:
+                    if j < n_boxes:
+                        keys[j] = True
+                    boxes[i] = None
+
+        if not (len(keys) > n_keys):
+            break
+
+    if n_keys == len(boxes):
+        canUnlockAll = True
+
+    return canUnlockAll
