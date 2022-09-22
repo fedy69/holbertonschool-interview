e9ed3029f9f70fc97f03fc3076792e62b1d1fdf9
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-09-22 22:32:21 +0100

    checks if a singly linked list has a cycle in it

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+  * @list: list to check.
+  * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *first_step = list, *second_step = list;
+
+	while (first_step && second_step && second_step->next)
+	{
+		first_step = first_step->next;
+		second_step = second_step->next->next;
+		if (first_step == second_step)
+			return (1);
+	}
+	return (0);
+}
