6a2b58d432f3cd3f36b04c972589d1cd04b4e48d
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-12-15 19:16:33 +0100

    linked list

--- 0x01-insert_in_sorted_linked_list/linked_lists.c
+++ 0x01-insert_in_sorted_linked_list/linked_lists.c
@@ -36,25 +36,27 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
     listint_t *new;
     listint_t *current;
 
-    current = *head;
+   
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+  current = *head;
 
-    new->n = n;
-    new->next = NULL;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+    return (NULL);
 
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
+new->n = n;
+new->next = NULL;
 
-    return (new);
+if (*head == NULL)
+    *head = new;
+else
+{
+    while (current->next != NULL)
+        current = current->next;
+    current->next = new;
+}
+
+return (new);
 }
 
 /**
