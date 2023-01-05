aa9bbb781feb4a374acba117074f61b74a106884
Fedy chihaoui <sasouciouchiha@gmail.com>
2023-01-05 22:43:47 +0100

    minimum operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,9 +6,7 @@ def minOperations(n: int) -> int:
     if n == 0:
         return 0
     operations = 0
-    
     while n & (n - 1) != 0:
         n -= n & -n
         operations += 1
-    
     return operations + (n.bit_length() - 1)
